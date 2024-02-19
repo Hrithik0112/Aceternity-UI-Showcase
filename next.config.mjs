a1216112b944010b8bb67f9f2a519d462fd6197c
Hrithik <slumdog4321@gmail.com>
2024-02-19 20:10:17 +0530

    next config update: domains to remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["flowbite.s3.amazonaws.com", "images.unsplash.com"],
+    //domains got depriciated in Next 14
+    // domains: ["flowbite.s3.amazonaws.com", "images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flowbite.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 };
 
